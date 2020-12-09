a9cd604ae85bb5c1fc6e94be9f4487f034627d89
Wesley <wesley.contato@hotmail.com>
2020-12-09 07:26:58 -0300

    Manipulando promoções e produtos

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Promocao.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -9,6 +9,20 @@ namespace Alura.Loja.Testes.ConsoleApp
         public string Descricao { get; internal set; }
         public DateTime DataInicio { get; internal set; }
         public DateTime DataTermino { get; internal set; }
-        public IList<PromocaoProduto> Produtos{ get; internal set; }
+        public IList<PromocaoProduto> Produtos { get; internal set; }
+
+        public Promocao()
+        {
+            this.Produtos = new List<PromocaoProduto>();
+        }
+
+
+        public void IncluiProduto(Produto produto)
+        {
+            this.Produtos.Add(new PromocaoProduto()
+            {
+                Produto = produto
+            });
+        }
     }
 }
