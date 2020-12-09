d635a048a71c243fca3793c9c5fda6984384ac96
Wesley <wesley.contato@hotmail.com>
2020-12-09 19:02:30 -0300

    Como realizar joins entre entidades relacionadas

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,6 +14,26 @@ namespace Alura.Loja.Testes.ConsoleApp
     class Program
     {
         static void Main(string[] args)
+        {
+            using (var contexto2 = new LojaContext())
+            {
+                var promocao = contexto2
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+
+
+                Console.WriteLine("\nMostrando os produtos da promoção:");
+                foreach (var item in promocao.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
+            }
+
+        }
+
+        private static void IncluirPromocao()
         {
             using (var contexto = new LojaContext())
             {
@@ -35,17 +56,6 @@ namespace Alura.Loja.Testes.ConsoleApp
 
                 contexto.SaveChanges();
             }
-
-            using (var contexto2 = new LojaContext())
-            {
-                var promocao = contexto2.Promocoes.FirstOrDefault();
-                Console.WriteLine("\nMostrando os produtos da promoção:");
-                foreach (var item in promocao.Produtos)
-                {
-                    Console.WriteLine(item.Produto);
-                }
-            }
-
         }
 
         private static void UmPraUm()
