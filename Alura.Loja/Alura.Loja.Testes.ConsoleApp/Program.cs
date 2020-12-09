8d6e4a49f80b5d0817836f6606b06837cbe30499
Wesley <wesley.contato@hotmail.com>
2020-12-09 19:23:32 -0300

    Corrigindo o modelo para recuperar objetos relacionados

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -15,6 +15,34 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var contexto = new LojaContext())
+            {
+                var cliente = contexto
+                    .Clientes
+                    .Include(c => c.EnderecoDeEntrega)
+                    .FirstOrDefault();
+
+                Console.WriteLine($"Endereço de entrega: {cliente.EnderecoDeEntrega}");
+
+                var produto = contexto
+                    .Produtos
+                    .Include(p => p.Compras)
+                    .Where(p => p.Id == 9004)
+                    .FirstOrDefault();
+
+                Console.WriteLine("Mostrando as compras do produto {produto.Nome");
+                foreach (var item in produto.Compras)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            }
+
+        }
+
+        private static void ExibeProdutosDaPromocao()
+        {
             using (var contexto2 = new LojaContext())
             {
                 var promocao = contexto2
@@ -30,7 +58,6 @@ namespace Alura.Loja.Testes.ConsoleApp
                     Console.WriteLine(item.Produto);
                 }
             }
-
         }
 
         private static void IncluirPromocao()
