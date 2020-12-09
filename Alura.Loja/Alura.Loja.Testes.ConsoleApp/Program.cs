9d34ac0ad745805a831978d205f33cb5cfaa87e1
Wesley <wesley.contato@hotmail.com>
2020-12-09 07:37:58 -0300

    Relacionamento um para um

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,7 +14,28 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var fulano = new Cliente();
+            fulano.Nome = "Fulanindo de tal";
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = 12,
+                Logradouro = "Rua dos Inválidos",
+                Complemento = "Sobrado",
+                Bairro = "Centro",
+                Cidade = "Cidade"
+            };
 
+            using (var contexto = new LojaContext())
+            {
+                contexto.Clientes.Add(fulano);
+            }
+            {
+
+            }
+        }
+
+        private static void MuitosParaMuitos()
+        {
             var p1 = new Produto() { Nome = "Suco de laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
             var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
             var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
@@ -36,8 +57,8 @@ namespace Alura.Loja.Testes.ConsoleApp
 
                 var promocao = contexto.Promocoes.Find(3);
                 contexto.Promocoes.Remove(promocao);
-                ExibeEntries(contexto.ChangeTracker.Entries());
-                                
+
+
                 contexto.SaveChanges();
             }
         }
